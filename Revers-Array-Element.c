e08084c1caf98446d9966095361f08e1a4bcf699
BHAGIRATH RAI <bhagirathr2005@gmail.com>
2025-09-16 09:37:33 +0530

    array

---
+++ Revers-Array-Element.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+ 
+    int Arr[5]={10,20,30,40,50};
+
+    for(int i=4;i>=0;i--){
+        printf("%d\n",Arr[i]);
+    }
+    
+
+
+    getch();
+}
