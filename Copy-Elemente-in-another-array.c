751f64fafe23f94fb34a616f2dab124c6dafa50a
Bhagirath2120 <bhagirathr2005@gami.com>
2025-09-16 17:19:42 +0530

    test.cppppp

--- Copy-Elemente-in-another-array.c
+++ Copy-Elemente-in-another-array.c
@@ -6,11 +6,11 @@ int A[5]={10,20,30,40,50};
 int B[5];
 for(int i=0;i<=4;i++)
 {
-   // printf("%d\n",A[i]);
+    printf("%d\n",A[i]);
        for(int j=0;j<=i;j++)
        {
         
-           printf("%d\n",&B[i]);
+           printf("%d\n",B[i]);
        }
     
 
