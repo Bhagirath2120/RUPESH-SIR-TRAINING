e5a08b080dffa73d0828f3f46af1d11410d47dd7
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-25 20:50:40 +0530

    array

--- array.c
+++ array.c
@@ -1,31 +1,32 @@
-<<<<<<< HEAD
-#include<stdio.h>
-int main(){
-    //int a=100;
-    //int b=101;
-    // printf("%d",arr[1]);
-    int arr[]={1,2,4,8,9};
+
+// #include<stdio.h>
+// int main(){
+//     //int a=100;
+//     //int b=101;
+//     // printf("%d",arr[1]);
+//     int arr[]={1,2,4,8,9};
    
 
-    for(int i = 4;i>0;i--){
+//     for(int i = 4;i>0;i--){
 
-    printf("%d",arr[i]);
-    }
+//     printf("%d",arr[i]);
+//     }
 
 
-    return 0;}
-=======
+//     return 0;}
+
+
+
 #include<stdio.h>
 
 int main(){
 
 int arr[5]={1,2,4};
-for(int i=0;i<3;i++){
-    printf("%d",arr[i]);
-}
+
+// printf("%d",arr[i]);
+
 return 0;
 
 
 
 }
->>>>>>> e08084c1caf98446d9966095361f08e1a4bcf699
